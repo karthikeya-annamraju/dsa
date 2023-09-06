3b8156367051eb22b301f3bd840eff57b115ad3d
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2023-09-06 20:50:13 +0530

    upr_lwr_bound.cpp

--- upr_lwr_bound.cpp
+++ upr_lwr_bound.cpp
@@ -6,39 +6,49 @@ using namespace std;
 int upperBound(int arr[], int size, int k) {
     int low = 0;
     int high = size - 1;
+    int ans;
     while (low < high) {
         int mid = (low + high)/2;
-        if (arr[mid] >= k) {
-            high = mid;
-        }
-        else {
+        if (arr[mid] == k) {
+            ans = mid;
             low = mid + 1;
         }
+        else if (arr[mid] < k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
     }
-    return low;
+    return ans;
 }
 
 int lowerBound(int arr[], int size, int k) {
     int low = 0;
     int high = size - 1;
-    while (low < high) {
+    int ans;
+    while (low <= high) {
         int mid = (low + high)/2;
-        if (arr[mid] <= k) {
-            low = mid + 1;
+        if (arr[mid] == k){
+            ans = mid;
+            high = mid - 1; 
+        }
+        else if (arr[mid] < k) {
+            low = mid+1;
         }
         else {
-            high = mid;
+            high = mid - 1;
         }
     }
-    return low;
+    return ans;
 }
 
 int main() {
     int arr[] ={1, 1, 2, 3, 3, 3, 3, 7};
     int k = 3;
     //cin >> k;
-    int lb = upperBound(arr, 8, k);
-    int ub = lowerBound(arr, 8, k);
-    cout << "lower bound is " << lb << endl;
-    cout << "upper bound is " << ub << endl;
+    int lrb = lowerBound(arr, 8, k);
+    int upb = upperBound(arr, 8, k);  
+    cout << "lower bound is " << lrb << endl;
+    cout << "upper bound is " << upb << endl;
 }
